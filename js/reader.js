ac159fcd73cb70774b723ea7914a9879f3ac3525
WalliCzech <118720285+WalliCzech@users.noreply.github.com>
2025-08-04 10:53:16 +0200

    Update reader.js

--- js/reader.js
+++ js/reader.js
@@ -126,7 +126,7 @@ window.startSynthomaReader = async (filePath) => {
                 }
                 return response.text();
             })
-            .then(htmlContent => typewriterWrite(htmlContent, typewriterContainer, signal))
+                        .then(htmlContent => typewriterWrite(htmlContent, typewriterContainer, signal))
             .then(() => {
                 if (window.animationManager) {
                     window.animationManager.initializeEffects(typewriterContainer);
